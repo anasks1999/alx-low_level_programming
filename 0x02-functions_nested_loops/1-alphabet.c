ba65d09ca1d395be6f2ccc33fd1a209dd654bc4b
Anasks19 <anasks1999@gmail.com>
2023-03-21 15:47:40 +0000

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowerCase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+}
