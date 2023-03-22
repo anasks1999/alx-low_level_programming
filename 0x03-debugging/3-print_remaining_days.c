51c15e18ff4e360433d5823f6bb7cd26f94942ad
Anasks19 <anasks1999@gmail.com>
2023-03-22 19:22:54 +0000

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0) && (year <= 2024))
 {
-if (month >= 2 && day >= 60)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -23,7 +23,7 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else if ((year % 400 == 0) && (year % 100 == 0) && (year <= 2024))
 {
-if (month >= 2 && day >= 60)
+if (month > 2 && day >= 60)
 {
 day++;
 }
