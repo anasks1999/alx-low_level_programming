e9a9e5c5b6d1f7aaeff6d9bcb0bb0f9304d90b45
Anasks19 <anasks1999@gmail.com>
2023-03-27 22:59:03 +0000

    Half lies they tell about me are not true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,22 +11,13 @@
 void puts2(char *str)
 {
 int i;
-int len = 0;
-int unpair = 0;
-
-while (*(str + len) != '\0')
-{
-len++;
-}
-len--;
 
 i = 0;
-while (*(str + i) <= *(str + len))
+while (*(str + i) != '\0')
 {
-if (unpair % 2 != 0 || unpair == 0)
+if (i % 2 == 0)
 _putchar(*(str + i));
 i++;
-unpair++;
 }
 _putchar('\n');
 }
