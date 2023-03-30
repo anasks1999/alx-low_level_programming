54db84de79b5755b8cb86e5492a729df5eb9cff0
Anasks19 <anasks1999@gmail.com>
2023-03-30 14:19:55 +0000

    Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,16 +9,26 @@
 
 char *cap_string(char *str)
 {
-int i = 0;
+int i;
+
+i = 0;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+}
 
 while (str[i])
 {
-if (*(str + i) >= 'a' && *(str + i) <= 'z')
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ','
+|| str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?'
+|| str[i] == '\"' || str[i] == '(' || str[i] == ')' || str[i] == '{'
+|| str[i] == '}')
 {
-str[i] += 'A' - 'a';
-}
-i++;
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+str[i + 1] -= 'a' - 'A';
 }
 
+i++;
+}
 return (str);
 }
