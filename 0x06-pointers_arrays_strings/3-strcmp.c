9e679875dbe8d68874cd48001c922bf7ca336b62
Anasks19 <anasks1999@gmail.com>
2023-03-30 11:44:27 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int str1;
-int str2;
+int i = 0;
 
-str1 = 0;
-str2 = 0;
+while (s1[i] == s2[i])
+{
+if (s1[i] == '\0')
+return (0);
+i++;
+}
 
-return (s1[str1] - s2[str2]);
+return (s1[i] - s2[i]);
 }
