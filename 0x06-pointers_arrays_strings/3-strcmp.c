cd55ec140d0cdd30fb3d5187f879b0f376433d23
Anasks19 <anasks1999@gmail.com>
2023-03-30 11:30:41 +0000

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: soustraction of n bytes.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int str1;
+int str2;
+
+str1 = 0;
+str2 = 0;
+
+return (s1[str1] - s2[str2]);
+}
