9be54b4c6035bc7360a68d64606c5cedb52a6aed
Anasks19 <anasks1999@gmail.com>
2023-04-03 15:00:50 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  * _strlen - measure length of string
+  * @s: the string
+  *
+  * Return: length of the string
+  */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (s[len])
+len++;
+return (len);
+}
+
+/**
+  * _strstr - locate a substring
+  * @haystack: string
+  * @needle: substring
+  *
+  * Return: begining of the location of @needle, otherwise NULL.
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+int needle_len = 0;
+int matches = 0;
+
+needle_len = _strlen(needle);
+while (*haystack && *needle)
+{
+if (*haystack != *needle)
+{
+needle -= matches;
+haystack -= (matches - 1);
+matches = 0;
+continue;
+}
+haystack++;
+needle++;
+matches++;
+if (matches == needle_len)
+break;
+}
+if (matches == needle_len)
+return (haystack - matches);
+return (NULL);
+}
